2e9aa8bac7172a3925b5813ec243d4b51203db3e
vusaladil <vusal_adil@gmail.com>
2020-01-05 14:34:22 -0600

    added more prints

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,5 +2,8 @@ public class AppTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Again");
+        System.out.println("Hello Again");
+        System.out.println("Hello Again");
+        
     }
 }

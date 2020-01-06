e6dc4a642c142c6bacd703a2ea6c8f1767cb779f
vusaladil <vusal_adil@gmail.com>
2020-01-05 21:18:27 -0600

    changed some numbers

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,9 +1,8 @@
 public class LoginTests {
     public static void main(String[] args) {
         System.out.println("Line 1");
-        System.out.println("Line 1");
-        System.out.println("Line 1");
-        System.out.println("Line 1");
-        System.out.println("Line 1");
+        System.out.println("Line 2");
+        System.out.println("Line 3");
+        System.out.println("Line 4");
     }
 }

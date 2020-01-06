af1a2923e20801da4532a83cfc4653c7431e608b
vusaladil <52684615+vusaladil@users.noreply.github.com>
2020-01-05 20:08:13 -0600

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,6 +1,8 @@
 public class LoginTests {
     public static void main(String[] args) {
-        System.out.println("Line 1");
-        System.out.println("Line 2");
+         System.out.println("Line 1");
+         System.out.println("Line 1");
+         System.out.println("Line 1");
+         System.out.println("Line 1");
     }
 }
